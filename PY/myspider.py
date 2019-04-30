b140f6eb73deecf903459774269d3bf39ea01173
Krissam Tapa <krissam.tapa@eastvantage-europe.com>
2019-04-30 20:57:35 +0800

    add scrapy

--- PY/myspider.py
+++ PY/myspider.py
@@ -2,14 +2,25 @@ import scrapy
 
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
-    start_urls = ['https://www.yelp.com/']
+    start_urls = ['https://www.businesslist.ph/location/manila']
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
+        for business_page in response.css('h4>a'):
+            yield response.follow(business_page,  callback=self.parse_info)
+            
+            
+        
+    def parse_info(self, response):
+        yield {
+            'title': response.css('#company_name::text').get(),
+            'address':  response.css('.location::text').get()
+        }
 
-        for next_page in response.css('a.next-posts-link'):
-            yield response.follow(next_page, self.parse)
 
 
-# https://scrapy.org/
+        # for next_page in response.css('a.pages_arrow'):
+        #     yield response.follow(next_page, self.parse)
+
+        # scrapy runspider myspider.py -o test.csv
+
+        
