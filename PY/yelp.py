962aa43f8c6a2695f2461941e2f060e57d07cd34
Krissam Tapa <krissam.tapa@eastvantage-europe.com>
2019-05-08 17:59:05 +0800

    Clean

--- PY/yelp.py
+++ PY/yelp.py
@@ -5,21 +5,32 @@ class BlogSpider(scrapy.Spider):
     start_urls = ['https://www.yelp.com/search?cflt=restaurants&find_loc=San+Francisco%2C+CA']
 
 
+    title ="test"
+    href = "test"
     def parse(self, response):
-        for business_page in response.css('h3>a'):
-            yield {
-                'title': response.css('h3 a::text').get().strip()
-                # 'link':  response.css('h3 a::href').get().strip()
-            }
+        for business_page in response.css('ul>li'):
+            if  business_page.css('h3>a::text').get():
+                BlogSpider.title = business_page.css('h3>a::text').get() 
+                BlogSpider.href = 'https://www.yelp.com' + business_page.css('h3>a::attr(href)').get().strip()
+                yield response.follow(business_page.css('h3>a::attr(href)').get().strip(),  callback=self.parse_info)
+                # yield {
+                #     'title': business_page.css('h3>a::text').get(),
+                #     'link': 'https://www.yelp.com' + business_page.css('h3>a::attr(href)').get().strip()
+                # }
+
+
+
             # yield response.follow(business_page,  callback=self.parse_info)
             
             
         
     def parse_info(self, response):
         yield {
-            'address':  response.css('.street-address address::text').get(),
-            'phone':  response.css('.biz-phone::text').get().strip(),
-            'website':  response.css('.biz-website a::text').get().strip()
+            'title': response.css(".biz-page-title::text").extract_first(),
+            'link': response.url,
+            'address':  response.css('.street-address address::text').extract(),
+            'phone':  response.css('.biz-phone::text').extract_first().strip(),
+            'website':  response.css('.biz-website a::text').extract_first().strip()
         }
 
 
@@ -29,6 +40,6 @@ class BlogSpider(scrapy.Spider):
         # for next_page in response.css('a.pages_arrow'):
         #     yield response.follow(next_page, self.parse)
 
-        # scrapy runspider myspider.py -o test.csv
+        # scrapy runspider yelp.py -o reports/test.csv
 
         
